b97d1257d4145b96ed4f186e05046be1bb0020e2
Sanket Naik <sanketnaik99@gmail.com>
2022-10-09 16:18:58 -0700

    Updates pragma block placement

--- dgemm-basic-omp.cpp
+++ dgemm-basic-omp.cpp
@@ -20,15 +20,15 @@ void square_dgemm(int n, double* A, double* B, double* C)
 
    // std::cout << "Insert your basic matrix multiply, openmp-parallel edition here " << std::endl;
 
-
-   LIKWID_MARKER_START(MY_MARKER_REGION_NAME);
+   #pragma omp parallel 
+   { LIKWID_MARKER_START(MY_MARKER_REGION_NAME)
    for (int i = 0; i < n; i++){
-      #pragma omp parallel for
+      #pragma omp for
       for (int j = 0; j < n; j++){
          for (int k = 0; k < n; k++){ 
             C[i + j*n] += A[i + k*n] * B[k + j*n];
          }
       }
    }
-   LIKWID_MARKER_STOP(MY_MARKER_REGION_NAME);
+   LIKWID_MARKER_STOP(MY_MARKER_REGION_NAME); }
 }
